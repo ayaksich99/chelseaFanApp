08a3efcccc3c5e1df22b014a2ff78a8ce2f742f4
ALEC_YAKSICH <ALEC_YAKSICH@midlandu.edu>
2020-10-08 09:51:37 -0500

    Playerinfo API working

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -9,33 +9,35 @@ import { HomePageService } from '../services/homepage.service';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent  implements OnInit {
-playerInfo: Object;
-players: Array<Player> = this.homepageservice.players
-first_name: string;
-last_name: string;
-constructor(private homepageservice: HomePageService, public dialog: MatDialog) { }
+export class HomePageComponent implements OnInit {
+  playerInfo: Object;
+  players: Array<Player> = this.homepageservice.players
+  first_name: string;
+  last_name: string;
+  constructor(private homepageservice: HomePageService, public dialog: MatDialog) { }
 
-searchPlayer(player_name: string){
-  this.homepageservice.playerSearch(player_name)
-  .subscribe((res: any) => {
-  
-    // If it's an array then pass the res.filter(...) to the dialog
-    if (res instanceof Array){
-      res = res.filter(p => p.player_name === player_name)[0];
-      console.log("filtering names");
-    }
-    this.dialog.open(DialogComponentComponent, {
-      data: res
+  searchPlayer(player_name: string) {
+    this.homepageservice.playerSearch(player_name)
+    .subscribe((res: any) => {
+        let first_name = player_name.split(" ")[0];
+        let last_name = player_name.split(" ")[1];
+        player_name = `${last_name} ${first_name}`
+        // If it's an array then pass the res.filter(...) to the dialog
+        if (res instanceof Array) {
+          res = res.filter(p => p.player_name === player_name)[0];
+          console.log("filtering names");
+        }
+        this.dialog.open(DialogComponentComponent, {
+          data: { res: res }
     })
-  });
+});
 }
 
 
 
 ngOnInit(): void {
-  
 
-  }
+
+}
 
 }
