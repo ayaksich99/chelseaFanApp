09cac865f1faa2e16ce4f76211e6f1a5b9208bc4
ALEC_YAKSICH <ALEC_YAKSICH@midlandu.edu>
2020-10-28 15:01:41 -0500

    Updated game

--- src/app/fixture/fixture.component.ts
+++ src/app/fixture/fixture.component.ts
@@ -11,7 +11,7 @@ const ELEMENT_DATA: Fixture[]= [
   {date:'Sat 17 Oct', kickoff: 	'15:00', match:	'Chelsea v Southampton', score:	'3 - 3', result:'D', competition:		'Premier League'},	
   {date:'Tue 20 Oct', kickoff: 	'19:45', match:	'Chelsea v Sevilla', score:	'0 - 0', result:'D', competition:		'Champions League'},
   {date:'Sat 24 Oct', kickoff: 	'17:30', match:	'Manchester United v Chelsea', score:'0 - 0'	, result:'D', competition:  'Premier League'},
-  {date:'Wed 28 Oct', kickoff: 	'19:45', match:	'Krasnodar v Chelsea', score:	'', result:'', competition:		'Champions League'},
+  {date:'Wed 28 Oct', kickoff: 	'19:45', match:	'Krasnodar v Chelsea', score:	'0 - 4', result:'W', competition:		'Champions League'},
   {date:'Sat 31 Oct', kickoff: 	'15:00', match:	'Burnley v Chelsea', score:	'', result:'', competition:		'Premier League'},	
   {date:'Wed 4 Nov', kickoff: 	'19:45', match:	'Chelsea v Rennes', score:'', result:'', competition:			'Champions League'},
   {date:'Sat 7 Nov', kickoff: 	'15:00', match:	'Chelsea v Sheffield United', score:'', result:	'', competition:		'Premier'},	
